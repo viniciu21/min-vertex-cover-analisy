fb0128a4f070fd9a4236cba4cdb91721e8ab7bc1
Vinicius Oliveira <vinicius.silva@afinz.com.br>
2022-07-19 22:08:58 -0300

    terminando

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # min-vertex-cover-analisy
 Projeto feito para a disciplina de Grafos com o intuito de analisar o algoritmo de cobertura de vertices 
+
+# Pre requisitos:
+
+Ter instalado o compilador do C++
+
+Deve-se rodar do visual studio code (por conta do settings.json que adiciona algumas libs ao C++)
+
+Para rodar deve-se estar com o terminal no diretorio do projeto digitar: 
+
+g++ .\vertex-cover.cpp -o vertex
+
+e depois .\vertex.exe nomeArquivo sendo o "nomeArquivo" o nome do arquivo que vai ser gerado com os dados.
--- vertex-cover.cpp
+++ vertex-cover.cpp
@@ -1,11 +1,8 @@
-
 #include <bits/stdc++.h>
 #include <fstream>
 #include <iomanip>
-#define maxn 100
+#define TAMANHO_MAXIMO_MATRIZ 100
 using namespace std;
-
-// #define resetarGrafo  memset(grafo, 0, sizeof grafo);
 
 class Graph
 {
@@ -15,13 +12,13 @@ public:
         this->resetarGrafo();
     }
 
-    bool grafo[maxn][maxn];
+    bool grafo[TAMANHO_MAXIMO_MATRIZ][TAMANHO_MAXIMO_MATRIZ];
 
-    void ajustarArray(bool arr[maxn][maxn])
+    void ajustarArray(bool arr[TAMANHO_MAXIMO_MATRIZ][TAMANHO_MAXIMO_MATRIZ])
     {
-        for (size_t i = 0; i < maxn; i++)
+        for (size_t i = 0; i < TAMANHO_MAXIMO_MATRIZ; i++)
         {
-            for (size_t j = 0; j < maxn; j++)
+            for (size_t j = 0; j < TAMANHO_MAXIMO_MATRIZ; j++)
             {
                 this->grafo[i][j] = arr[i][j];
             }
@@ -30,9 +27,9 @@ public:
 
     void printMatrix()
     {
-        for (size_t i = 0; i < maxn; i++)
+        for (size_t i = 0; i < TAMANHO_MAXIMO_MATRIZ; i++)
         {
-            for (size_t j = 0; j < maxn; j++)
+            for (size_t j = 0; j < TAMANHO_MAXIMO_MATRIZ; j++)
             {
                 cout << this->grafo[i][j];
             }
@@ -48,7 +45,13 @@ public:
 
     void resetarGrafo()
     {
-        memset(grafo, 0, sizeof grafo);
+        for (size_t i = 0; i < TAMANHO_MAXIMO_MATRIZ; i++)
+        {
+            for (size_t j = 0; j < TAMANHO_MAXIMO_MATRIZ; j++)
+            {
+                this->grafo[i][j] = 0;
+            }
+        }
     }
 };
 
@@ -62,13 +65,19 @@ int recalcularinicio(int inicio)
 bool coberto(int arestas, int meio, int vertices, bool grafo[100][100])
 {
     bool result = false;
-    int inicio = (1 * pow(2, meio)) - 1;
-    int fim = (1 * pow(2, arestas));
-    bool grafoDePara[maxn][maxn];
+    int inicio = pow(2, meio) - 1;
+    int fim = pow(2, arestas);
+    bool grafoDePara[TAMANHO_MAXIMO_MATRIZ][TAMANHO_MAXIMO_MATRIZ];
 
     while (inicio < fim)
     {
-        memset(grafoDePara, 0, sizeof grafoDePara);
+        for (size_t m = 0; m < TAMANHO_MAXIMO_MATRIZ; m++)
+        {
+            for (size_t n = 0; n < TAMANHO_MAXIMO_MATRIZ; n++)
+            {
+                grafoDePara[m][n] = 0;
+            }
+        }
         int contadorDeVertices = 0;
 
         for (int j = 1, v = 1; j < fim; j = j * 2, v++)
@@ -101,17 +110,19 @@ bool coberto(int arestas, int meio, int vertices, bool grafo[100][100])
 
 int NumCoberturaMinima(int arestas, int vertices, bool grafo[100][100])
 {
-    int inicio = 1, fim = arestas;
-    while (fim > inicio)
+    int numVertexCover = 1, indexArestas = arestas;
+    bool isCoberto;
+    while (indexArestas > numVertexCover)
     {
-        int meio = (inicio + fim) / 2;
-        if (coberto(arestas, meio, vertices, grafo) == false)
-            inicio = meio + 1;
+        int meio = (numVertexCover + indexArestas) / 2;
+        isCoberto = coberto(arestas, meio, vertices, grafo);
+        if (isCoberto == false)
+            numVertexCover = meio + 1;
         else
-            fim = meio;
+            indexArestas = meio;
     }
 
-    return inicio;
+    return numVertexCover;
 }
 
 int main(int argc, char **argv)
@@ -164,20 +175,19 @@ int main(int argc, char **argv)
 
     cout << "5: 15 arestas e 14 vertices" << endl;
 
-    cout << "6: 6 arestas e 6 vertices" << endl;
+    cout << "6: 18 arestas e 13 vertices" << endl;
 
-    cout << "7: 6 arestas e 6 vertices" << endl;
+    cout << "7: 12 arestas e 6 vertices" << endl;
 
-    cout << "8: 6 arestas e 6 vertices" << endl;
+    cout << "8: 8 arestas e 8 vertices" << endl;
 
-    cout << "9: 6 arestas e 6 vertices" << endl;
+    cout << "9: 9 arestas e 9 vertices" << endl;
 
-    cout << "10: 6 arestas e 6 vertices" << endl;
+    cout << "10: 10 arestas e 10 vertices" << endl;
 
     cin >> choice;
-    // bool array[100][100];
-    // memset(array, 0, sizeof array);
-    while (choice >= 10)
+
+    while (choice > 10 || choice < 0)
     {
         cout << "Por favor, escolha uma opção entre 1 e 10" << endl;
         cin >> choice;
@@ -185,7 +195,6 @@ int main(int argc, char **argv)
     int arestas, vertices;
 
     Graph *gph = new Graph();
-
     switch (choice)
     {
     case 1:
@@ -233,6 +242,7 @@ int main(int argc, char **argv)
         gph->adicionarAresta(2, 5);
         gph->adicionarAresta(3, 6);
         gph->adicionarAresta(3, 7);
+        break;
     }
     case 4:
     {
@@ -251,11 +261,13 @@ int main(int argc, char **argv)
         gph->adicionarAresta(6, 5);
         gph->adicionarAresta(6, 10);
         gph->adicionarAresta(5, 10);
+        break;
     }
-    case 5: {
+    case 5:
+    {
         arestas = 15;
         vertices = 14;
-        
+
         gph->adicionarAresta(1, 5);
         gph->adicionarAresta(1, 4);
         gph->adicionarAresta(1, 8);
@@ -271,56 +283,156 @@ int main(int argc, char **argv)
         gph->adicionarAresta(12, 3);
         gph->adicionarAresta(12, 6);
         gph->adicionarAresta(10, 11);
+        break;
+    }
+    case 6:
+    {
+        arestas = 18;
+        vertices = 13;
 
+        gph->adicionarAresta(1, 7);
+        gph->adicionarAresta(1, 2);
+        gph->adicionarAresta(7, 2);
+        gph->adicionarAresta(7, 3);
+        gph->adicionarAresta(3, 2);
+        gph->adicionarAresta(3, 10);
+        gph->adicionarAresta(10, 2);
+        gph->adicionarAresta(2, 4);
+        gph->adicionarAresta(10, 12);
+        gph->adicionarAresta(12, 6);
+        gph->adicionarAresta(6, 4);
+        gph->adicionarAresta(4, 5);
+        gph->adicionarAresta(1, 5);
+        gph->adicionarAresta(1, 9);
+        gph->adicionarAresta(9, 11);
+        gph->adicionarAresta(11, 8);
+        gph->adicionarAresta(8, 12);
+        gph->adicionarAresta(11, 13);
+        gph->adicionarAresta(13, 8);
+        gph->adicionarAresta(13, 12);
+        break;
+    }
+    case 7:
+    {
+        arestas = 12;
+        vertices = 6;
+
+        gph->adicionarAresta(1, 2);
+        gph->adicionarAresta(1, 3);
+        gph->adicionarAresta(1, 5);
+        gph->adicionarAresta(1, 6);
+        gph->adicionarAresta(5, 6);
+        gph->adicionarAresta(5, 2);
+        gph->adicionarAresta(5, 4);
+        gph->adicionarAresta(6, 4);
+        gph->adicionarAresta(6, 3);
+        gph->adicionarAresta(2, 3);
+        gph->adicionarAresta(2, 4);
+        gph->adicionarAresta(3, 4);
+        break;
+    }
+    case 8:
+    {
+        arestas = 8;
+        vertices = 8;
+
+        gph->adicionarAresta(1, 3);
+        gph->adicionarAresta(2, 4);
+        gph->adicionarAresta(7, 4);
+        gph->adicionarAresta(3, 5);
+        gph->adicionarAresta(5, 7);
+        gph->adicionarAresta(6, 8);
+        gph->adicionarAresta(8, 3);
+        gph->adicionarAresta(1, 2);
+        break;
+    }
+    case 9:
+    {
+        arestas = 9;
+        vertices = 9;
+
+        gph->adicionarAresta(1, 3);
+        gph->adicionarAresta(1, 2);
+        gph->adicionarAresta(3, 9);
+        gph->adicionarAresta(9, 8);
+        gph->adicionarAresta(6, 8);
+        gph->adicionarAresta(6, 4);
+        gph->adicionarAresta(4, 3);
+        gph->adicionarAresta(4, 5);
+        gph->adicionarAresta(4, 7);
+
+        break;
+    }
+    case 10:
+    {
+        arestas = 10;
+        vertices = 10;
+
+        gph->adicionarAresta(10, 1);
+        gph->adicionarAresta(10, 2);
+        gph->adicionarAresta(10, 3);
+        gph->adicionarAresta(10, 4);
+        gph->adicionarAresta(10, 5);
+        gph->adicionarAresta(10, 6);
+        gph->adicionarAresta(10, 7);
+        gph->adicionarAresta(10, 8);
+        gph->adicionarAresta(10, 9);
+        gph->adicionarAresta(10, 10);
+
+        break;
     }
     }
     auto inicioExe = std::chrono::high_resolution_clock::now();
     int numCoberturaMinimaResult = NumCoberturaMinima(arestas, vertices, gph->grafo);
-    auto resultado = std::chrono::high_resolution_clock::now() - inicioExe; 
+    auto resultado = std::chrono::high_resolution_clock::now() - inicioExe;
     long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(resultado).count();
-    
-    cout
-            << left
-            << setw(20)
-            << "|"
-            << to_string(numCoberturaMinimaResult)
-            << left
-            << setw(20)
-            << "| "
-            << to_string(microseconds)
-            << left
-            << setw(20)
-            << "| "
-            << to_string(vertices)
-            << left
-            << setw(20)
-            << "|" + to_string(arestas) + "|"
-            << left
-            << setw(20)
-            << " Melhor Solução |"
-            << endl;
 
-     GraphFile.open(filename, ios_base::openmode::_S_app);
-     GraphFile
-            << right
-            << setw(10)
-            << "|"
-            << to_string(numCoberturaMinimaResult)
-            << right
-            << setw(20)
-            << "| "
-            << to_string(microseconds)
-            << right
-            << setw(20)
-            << "| "
-            << to_string(vertices)
-            << right
-            << setw(20)
-            << "|" + to_string(arestas) + "|"
-            << right
-            << setw(10)
-            << " Melhor Solução |"
-            << endl;
+    cout
+        << left
+        << "|"
+        << setw(10)
+        << to_string(numCoberturaMinimaResult)
+        << setw(10)
+        << right
+        << "| "
+        << setw(10)
+        << to_string(microseconds)
+        << left
+        << setw(10)
+        << "| "
+        << setw(10)
+        << to_string(vertices)
+        << left
+        << setw(10)
+        << "|" + to_string(arestas) + "|"
+        << left
+        << setw(10)
+        << " Melhor Solução |"
+        << endl;
+
+    GraphFile.open(filename, ios_base::openmode::_S_app);
+    GraphFile
+        << right
+        << setw(10)
+        << "|"
+        << to_string(numCoberturaMinimaResult)
+        << right
+        << setw(20)
+        << "| "
+        << to_string(microseconds)
+        << right
+        << setw(20)
+        << "| "
+        << to_string(vertices)
+        << right
+        << setw(20)
+        << "|" + to_string(arestas)
+        << right
+        << setw(10)
+        << "|"
+        << setw(10)
+        << " Melhor Solução |"
+        << endl;
 
     GraphFile.close();
 
